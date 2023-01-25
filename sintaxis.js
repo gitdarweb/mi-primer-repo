45e2e88a4799164624cbfc877fc53885eb6b01de
Dario Duarte <duartedario780@gmail.com>
2023-01-25 20:58:45 -0300

    Mi primer CV

--- sintaxis.js
+++ sintaxis.js
@@ -5,7 +5,7 @@ const datos = [
     {dato:"ciudad:", caract:"La Plata."},
     {dato:"Pais:", caract:"Argentina."},
     {dato:"whatsapp:", caract:"+54-221-3533461."},
-    {dato:"Mail:", caract:"duartedario780@gmail.com"}
+    {dato:"E-mail:", caract:"duartedario780@gmail.com"}
   ];
   
   function displayDatos() {
@@ -20,29 +20,42 @@ const datos = [
   }
   //--------------------------------------------------------------------//
   const datos1 = [
-    {dato1:"Nombre y Apellido:", caract1:"Duarte Dario."},
-    {dato1:"edad:", caract1:"48."},
-   
+    {dato1:"Estudio secundario:", caract1:"Bachillerato Las Artes, especializado en Artes Plasticas."},
+    {dato1:"Estudio Terciario:", caract1:"Cursando 1er año en la carrera de Analista Programador Universitario,(U.N.L.P.)."},
+    {dato1:"Dirección Provincial de Educación:", caract1:"Programación de sistemas informáticos multimediales(2022)."},
+    {dato1:"Dirección Provincial de Educación:", caract1:"Reparación de celulares."},
+    {dato1:"Punto Digital, Municipalidad de La Plata:", caract1:"Marketing Digital."},
+    {dato1:"Facultad de Ingeniería U.N.L.P.:", caract1:"Operador de PC nivel intermedio."},
+    {dato1:"Asociación de Marcas y Franquicias:", caract1:"Convención Nacional 2010-2011-2012."}
   ];
   function displayDatos1() {
     document.getElementById("demo1").innerHTML =
     datos1[0].dato1 + " " + datos1[0].caract1 + "<br>" +
-    datos1[1].dato1 + " " + datos1[1].caract1;
+    datos1[1].dato1 + " " + datos1[1].caract1 + "<br>" +
+    datos1[2].dato1 + " " + datos1[2].caract1 + "<br>" +
+    datos1[3].dato1 + " " + datos1[3].caract1 + "<br>" +
+    datos1[4].dato1 + " " + datos1[4].caract1 + "<br>" +
+    datos1[5].dato1 + " " + datos1[5].caract1 + "<br>" +
+    datos1[6].dato1 + " " + datos1[6].caract1;
     
   }
   
   //----------------------------------------------------------------------//
   
   const datos2 = [
-    {dato2:"Nombre y Apellido:", caract2:"Duarte Dario."},
-    {dato2:"edad:", caract2:"48."},
-   
+    {dato2:"El Noble Repulgue:", caract2:"Dueño Franquizia sucursal, La Plata2."},
+    {dato2:"El Noble:", caract2:"Cambio de Firma : Cepas Argentina."},
+    {dato2:"Nuevo Modelo de Negocio:", caract2:"Gerenciando y batiendo record en ventas."},
+    {dato2:"Manejo de Personal", caract2:"Sistema Moderno de negocios, desde la tecnología y el Marketing."},
+    {dato2:"Desde 1998,", caract2:"Hasta 2018."}
   ];
-  function displayDatos2() {
+   function displayDatos2() {
     document.getElementById("demo2").innerHTML =
-    datos2[0].dato1 + " " + datos2[0].caract1 + "<br>" +
-    datos2[1].dato1 + " " + datos2[1].caract1;
-    
+    datos2[0].dato2 + " " + datos2[0].caract2 + "<br>" +
+    datos2[1].dato2 + " " + datos2[1].caract2 + "<br>" +
+    datos2[2].dato2 + " " + datos2[2].caract2 + "<br>" +
+    datos2[3].dato2 + " " + datos2[3].caract2 + "<br>" +
+    datos2[4].dato2 + " " + datos2[4].caract2;
   }
   
   
